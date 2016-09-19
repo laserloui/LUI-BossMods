324fcdf8725717f8bdf03d3e61f665123da9dd9f
Loui-PC\Loui <loui@readycheck.org>
2016-09-19 11:43:39 +0200

    Adjusted module to new dynamic settings routine

--- Encounters/DS/SystemDaemons.lua
+++ Encounters/DS/SystemDaemons.lua
@@ -18,6 +18,10 @@ local Locales = {
         ["alert.disconnect_south"] = "Disconnect South!",
         ["alert.purge_player"] = "Purge on you!",
         ["alert.interrupt"] = "Interrupt!",
+        -- Labels
+        ["label.disconnect_north"] = "Disconnect North",
+        ["label.disconnect_south"] = "Disconnect South",
+        ["label.purge"] = "Purge",
     },
     ["deDE"] = {},
     ["frFR"] = {},
@@ -48,26 +52,68 @@ function Mod:new(o)
     self.runtime = {}
     self.config = {
         enable = true,
-        purge = {
-            enable = false,
-            thickness = 7,
-            alert = "Large",
-            sound = "alert",
-            color = "ffff4500",
+        units = {
+            north = {
+                enable = true,
+                label = "unit.boss_north",
+            },
+            south = {
+                enable = true,
+                label = "unit.boss_south",
+            },
         },
-        disconnect = {
-            enable = true,
-            cast = true,
-            alert = "Large",
-            sound = "alarm",
-            color = "ff9932cc",
+        casts = {
+            disconnect = {
+                enable = true,
+                color = "ff9932cc",
+                label = "cast.disconnect"
+            },
+            powersurge = {
+                enable = true,
+                color = "ffff4500",
+                label = "cast.power_surge",
+            },
         },
-        powersurge = {
-            enable = true,
-            cast = true,
-            alert = "Large",
-            sound = "alert",
-            color = "ffff4500",
+        alerts = {
+            disconnect = {
+                enable = true,
+                color = "ff9932cc",
+                label = "cast.disconnect"
+            },
+            powersurge = {
+                enable = true,
+                color = "ffff4500",
+                label = "cast.power_surge",
+            },
+            purge = {
+                enable = true,
+                label = "label.purge",
+            },
+        },
+        sounds = {
+            disconnect = {
+                enable = true,
+                file = "info",
+                label = "cast.disconnect"
+            },
+            powersurge = {
+                enable = true,
+                file = "alert",
+                label = "cast.power_surge",
+            },
+            purge = {
+                enable = false,
+                file = "beware",
+                label = "label.purge",
+            },
+        },
+        lines = {
+            purge = {
+                enable = true,
+                color = "ffff4500",
+                thickness = 7,
+                label = "label.purge",
+            },
         },
     }
     return o
@@ -86,55 +132,45 @@ function Mod:OnUnitCreated(nId, tUnit, sName, bInCombat)
     end
 
     if sName == self.L["unit.boss_north"] and bInCombat == true then
-        self.core:AddUnit(nId,sName,tUnit,true,true,false,false,"N",self.config.northColor)
+        self.core:AddUnit(nId,sName,tUnit,self.config.units.north.enable,true,false,false,"N",self.config.units.north.color)
     elseif sName == self.L["unit.boss_south"] and bInCombat == true then
-        self.core:AddUnit(nId,sName,tUnit,true,true,false,false,"S",self.config.southColor)
+        self.core:AddUnit(nId,sName,tUnit,self.config.units.north.enable,true,false,false,"S",self.config.units.south.color)
     end
 end
 
 function Mod:OnCastStart(nId, sCastName, tCast, sName)
     if sCastName == self.L["cast.disconnect"] then
-        if self.config.disconnect.enable == true then
+        if self.config.casts.disconnect.enable == true then
             if sName == self.L["unit.boss_north"] then
-                if self.config.disconnect.sound ~= "None" then
-                    self.core:PlaySound(self.config.disconnect.sound)
-                end
-
-                if self.config.disconnect.cast == true then
-                    self.core:ShowCast(tCast,(sCastName.." North"),self.config.disconnect.color)
-                end
-
-                if self.config.disconnect.alert ~= "None" then
-                    self.core:ShowAlert(sName.."_disconnect", self.L["alert.disconnect_north"])
-                end
+                self.core:ShowCast(tCast,self.L["label.disconnect_north"],self.config.casts.disconnect.color)
             elseif sName == self.L["unit.boss_south"] then
-                if self.config.disconnect.sound ~= "None" then
-                    self.core:PlaySound(self.config.disconnect.sound)
-                end
-
-                if self.config.disconnect.cast == true then
-                    self.core:ShowCast(tCast,(sCastName.." South"),self.config.disconnect.color)
-                end
-
-                if self.config.disconnect.alert ~= "None" then
-                    self.core:ShowAlert(sName.."_disconnect", self.L["alert.disconnect_south"])
-                end
+                self.core:ShowCast(tCast,self.L["label.disconnect_south"],self.config.casts.disconnect.color)
             end
         end
+
+        if self.config.alerts.disconnect.enable == true then
+            if sName == self.L["unit.boss_north"] then
+                self.core:ShowAlert("disconnect_"..tostring(nId), self.L["alert.disconnect_north"],self.config.alerts.disconnect.duration,self.config.alerts.disconnect.color)
+            elseif sName == self.L["unit.boss_south"] then
+                self.core:ShowAlert("disconnect_"..tostring(nId), self.L["alert.disconnect_south"],self.config.alerts.disconnect.duration,self.config.alerts.disconnect.color)
+            end
+        end
+
+        if self.config.sounds.disconnect.enable == true then
+            self.core:PlaySound(self.config.sounds.disconnect.file)
+        end
     elseif sCastName == self.L["cast.power_surge"] then
-        if self.config.powersurge.enable == true then
-            if self.core:GetDistance(tCast.tUnit) < 25 then
-                if self.config.powersurge.cast == true then
-                    self.core:ShowCast(tCast,sCastName,self.config.powersurge.color)
-                end
+        if self.core:GetDistance(tCast.tUnit) < 25 then
+            if self.config.casts.powersurge.enable == true then
+                self.core:ShowCast(tCast,sCastName,self.config.casts.powersurge.color)
+            end
 
-                if self.config.powersurge.sound ~= "None" then
-                    self.core:PlaySound(self.config.powersurge.sound)
-                end
+            if self.config.sounds.powersurge.enable == true then
+                self.core:PlaySound(self.config.sounds.powersurge.file)
+            end
 
-                if self.config.powersurge.alert ~= "None" then
-                    self.core:ShowAlert(sName.."_power_surge", self.L["alert.interrupt"])
-                end
+            if self.config.alerts.powersurge.enable == true then
+                self.core:ShowAlert("power_surge_"..tostring(nId), self.L["alert.interrupt"],self.config.alerts.powersurge.duration,self.config.alerts.powersurge.color)
             end
         end
     end
@@ -142,27 +178,25 @@ end
 
 function Mod:OnBuffAdded(nId, nSpellId, sName, tData, sUnitName, nStack, nDuration)
     if DEBUFF__PURGE == nSpellId then
-        if self.config.purge.enable == true then
-            if tData.tUnit:IsThePlayer() then
-                if self.config.purge.sound ~= "None" then
-                    self.core:PlaySound(self.config.purge.sound)
-                end
-
-                if self.config.purge.alert ~= "None" then
-                    self.core:ShowAlert(tostring(nId).."_purge", self.L["alert.purge_player"])
-                end
+        if tData.tUnit:IsThePlayer() then
+            if self.config.sounds.purge.enable == true then
+                self.core:PlaySound(self.config.sounds.purge.file)
             end
 
-            self.core:DrawPolygon(tostring(nId).."_purge", tData.tUnit, 6, 0, self.config.purge.thickness, self.config.purge.color, 20, nDuration)
+            if self.config.alerts.purge.enable == true then
+                self.core:ShowAlert("purge_"..tostring(nId), self.L["alert.purge_player"],self.config.alerts.purge.duration,self.config.alerts.purge.color)
+            end
+        end
+
+        if self.config.lines.purge.enable == true then
+            self.core:DrawPolygon("purge_"..tostring(nId), tData.tUnit, 6, 0, self.config.lines.purge.thickness, self.config.lines.purge.color, 20, nDuration)
         end
     end
 end
 
 function Mod:OnBuffRemoved(nId, nSpellId, sName, tData, sUnitName)
     if DEBUFF__PURGE == nSpellId then
-        if self.config.purge.enable == true then
-            self.core:RemovePolygon(tostring(nId).."_purge")
-        end
+        self.core:RemovePolygon("purge_"..tostring(nId))
     end
 end
 
