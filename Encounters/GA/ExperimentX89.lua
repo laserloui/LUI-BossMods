be6cc0a018c6b8a0873ca3aaff608205b1b74767
Loui-PC\Loui <loui@readycheck.org>
2016-09-19 10:20:45 +0200

    Added placeholder boss modules for Genetic Archives.

---
+++ Encounters/GA/ExperimentX89.lua
@@ -0,0 +1,71 @@
+require "Window"
+require "Apollo"
+
+local Mod = {}
+local LUI_BossMods = Apollo.GetAddon("LUI_BossMods")
+local Encounter = "ExperimentX89"
+
+local Locales = {
+    ["enUS"] = {},
+    ["deDE"] = {},
+    ["frFR"] = {},
+}
+
+function Mod:new(o)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+    self.instance = "Genetic Archives"
+    self.displayName = "Experiment X-89"
+    self.tTrigger = {
+        sType = "ANY",
+        tZones = {
+            [1] = {
+                continentId = 67,
+                parentZoneId = 147,
+                mapId = 148,
+            },
+        },
+        tNames = {
+            ["enUS"] = {"Experiment X-89"},
+        },
+    }
+    self.run = false
+    self.runtime = {}
+    self.config = {
+        enable = true,
+    }
+    return o
+end
+
+function Mod:Init(parent)
+    Apollo.LinkAddon(parent, self)
+
+    self.core = parent
+    self.L = parent:GetLocale(Encounter,Locales)
+end
+
+function Mod:OnUnitCreated(nId, tUnit, sName, bInCombat)
+    if not self.run == true then
+        return
+    end
+end
+
+function Mod:IsRunning()
+    return self.run
+end
+
+function Mod:IsEnabled()
+    return self.config.enable
+end
+
+function Mod:OnEnable()
+    self.run = true
+end
+
+function Mod:OnDisable()
+    self.run = false
+end
+
+local ModInst = Mod:new()
+LUI_BossMods.modules[Encounter] = ModInst
---
+++ Encounters/GA/Phagemaw.lua
@@ -0,0 +1,71 @@
+require "Window"
+require "Apollo"
+
+local Mod = {}
+local LUI_BossMods = Apollo.GetAddon("LUI_BossMods")
+local Encounter = "PhageMaw"
+
+local Locales = {
+    ["enUS"] = {},
+    ["deDE"] = {},
+    ["frFR"] = {},
+}
+
+function Mod:new(o)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+    self.instance = "Genetic Archives"
+    self.displayName = "Phage Maw"
+    self.tTrigger = {
+        sType = "ANY",
+        tZones = {
+            [1] = {
+                continentId = 67,
+                parentZoneId = 147,
+                mapId = 149,
+            },
+        },
+        tNames = {
+            ["enUS"] = {"Phage Maw"},
+        },
+    }
+    self.run = false
+    self.runtime = {}
+    self.config = {
+        enable = true,
+    }
+    return o
+end
+
+function Mod:Init(parent)
+    Apollo.LinkAddon(parent, self)
+
+    self.core = parent
+    self.L = parent:GetLocale(Encounter,Locales)
+end
+
+function Mod:OnUnitCreated(nId, tUnit, sName, bInCombat)
+    if not self.run == true then
+        return
+    end
+end
+
+function Mod:IsRunning()
+    return self.run
+end
+
+function Mod:IsEnabled()
+    return self.config.enable
+end
+
+function Mod:OnEnable()
+    self.run = true
+end
+
+function Mod:OnDisable()
+    self.run = false
+end
+
+local ModInst = Mod:new()
+LUI_BossMods.modules[Encounter] = ModInst
