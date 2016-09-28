5f032e53cdd2c836079af9dd9655334205c198bf
Loui-PC\Loui <loui@readycheck.org>
2016-09-28 19:03:18 +0200

    Changed Trigger Type from ANY to ALL.

--- Encounters/RMT/Engineers.lua
+++ Encounters/RMT/Engineers.lua
@@ -63,7 +63,7 @@ function Mod:new(o)
     self.instance = "Redmoon Terror"
     self.displayName = "Engineers"
     self.tTrigger = {
-        sType = "ANY",
+        sType = "ALL",
         tZones = {
             [1] = {
                 continentId = 104,
