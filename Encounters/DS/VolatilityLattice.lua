e015e21061d19667c7db5cbe2ae2faf754322a3b
Loui-PC\Loui <loui@readycheck.org>
2016-10-21 22:56:24 +0200

    Added Volatility module.

--- Encounters/DS/VolatilityLattice.lua
+++ Encounters/DS/VolatilityLattice.lua
@@ -6,9 +6,47 @@ local LUI_BossMods = Apollo.GetAddon("LUI_BossMods")
 local Encounter = "VolatilityLattice"
 
 local Locales = {
-    ["enUS"] = {},
+    ["enUS"] = {
+        -- Units
+        ["unit.boss"] = "Avatus",
+        ["unit.devourer"] = "Data Devourer",
+        -- Datachron
+        ["datachron.laser"] = "Avatus sets his focus on (.*)!",
+        ["datachron.delete_all"] = "Avatus prepares to delete all data!",
+        ["datachron.secure_sector"] = "The Secure Sector Enhancement Ports have been activated!",
+        ["datachron.locomotion"] = "The Vertical Locomotion Enhancement Ports have been activated!",
+        -- Alerts
+        ["alert.laser"] = "LASER ON %s!",
+        ["alert.laser_player"] = "LASER ON YOU!",
+        -- Messages
+        ["message.laser"] = "Next Laser",
+        ["message.pillar"] = "Next Pillars",
+        ["message.devourer"] = "Next Devourers",
+        -- Labels
+        ["label.laser"] = "Laser",
+        ["label.pillar"] = "Pillar",
+    },
     ["deDE"] = {},
-    ["frFR"] = {},
+    ["frFR"] = {
+        -- Units
+        ["unit.boss"] = "Avatus",
+        ["unit.devourer"] = "Dévoreur de données",
+        -- Datachron
+        ["datachron.laser"] = "Avatus sets his focus on (.*)!", -- Missing!
+        ["datachron.delete_all"] = "Avatus se prépare à effacer toutes les données !",
+        ["datachron.secure_sector"] = "Les ports d'amélioration de secteur sécurisé ont été activés !",
+        ["datachron.locomotion"] = "Les ports d'amélioration de locomotion verticale ont été activés !",
+        -- Alerts
+        ["alert.laser"] = "LASER SUR %s!",
+        ["alert.laser_player"] = "LASER SUR TOI!",
+        -- Messages
+        ["message.laser"] = "Prochain Laser",
+        ["message.pillar"] = "Prochain Pillier",
+        ["message.devourer"] = "Prochain Dévoreur",
+        -- Labels
+        ["label.laser"] = "Laser",
+        ["label.pillar"] = "Pillier",
+    },
 }
 
 function Mod:new(o)
@@ -18,6 +56,8 @@ function Mod:new(o)
     self.instance = "Datascape"
     self.displayName = "Volatility Lattice"
     self.tTrigger = {
+        sType = "ANY",
+        tNames = {"unit.boss"},
         tZones = {
             [1] = {
                 continentId = 52,
@@ -30,6 +70,63 @@ function Mod:new(o)
     self.runtime = {}
     self.config = {
         enable = true,
+        timers = {
+            devourer = {
+                enable = true,
+                position = 1,
+                label = "unit.devourer",
+            },
+            pillar = {
+                enable = true,
+                position = 2,
+                label = "label.pillar",
+            },
+            laser = {
+                enable = true,
+                position = 3,
+                label = "label.laser",
+            },
+        },
+        alerts = {
+            laser = {
+                enable = true,
+                label = "label.laser",
+            },
+        },
+        sounds = {
+            laser = {
+                enable = true,
+                file = "alert",
+                label = "label.laser",
+            },
+        },
+        auras = {
+            laser = {
+                enable = true,
+                sprite = "LUIBM_run",
+                color = "ffff4500",
+                label = "label.laser",
+            },
+        },
+        icons = {
+            laser = {
+                enable = true,
+                sprite = "LUIBM_crosshair",
+                size = 80,
+                color = "ffff4500",
+                label = "label.laser",
+            },
+        },
+        lines = {
+            devourer = {
+                enable = true,
+                priority = 1,
+                thickness = 6,
+                max = 60,
+                color = "ff00ffff",
+                label = "unit.devourer",
+            },
+        },
     }
     return o
 end
@@ -45,6 +142,53 @@ function Mod:OnUnitCreated(nId, tUnit, sName, bInCombat)
     if not self.run == true then
         return
     end
+
+    if sName == self.L["unit.devourer"] then
+        self.core:DrawLineBetween(nId, tUnit, nil, self.config.lines.devourer)
+
+        local nTick = Apollo.GetTickCount()
+        if (self.nLastDevourer + 13000) < nTick then
+            self.nLastDevourer = nTick
+            self.core:AddTimer("Timer_Devourer", self.L["message.devourer"], 15, self.config.timers.devourer)
+        end
+    end
+end
+
+function Mod:OnUnitDestroyed(nId, tUnit, sName)
+    if sName == self.L["unit.devourer"] then
+        self.core:RemoveLineBetween(nId)
+    end
+end
+
+function Mod:OnDatachron(sMessage, sSender, sHandler)
+    if sMessage:match(self.L["datachron.delete_all"]) then
+        self.core:AddTimer("Timer_Pillar", self.L["message.pillar"], 50, self.config.timers.pillar)
+    elseif sMessage:match(self.L["datachron.secure_sector"]) then
+        self.core:AddTimer("Timer_Devourer", self.L["message.devourer"], 53, self.config.timers.devourer)
+        self.core:AddTimer("Timer_Pillar", self.L["message.pillar"], 58, self.config.timers.pillar)
+        self.core:AddTimer("Timer_Laser", self.L["message.laser"], 44, self.config.timers.laser)
+    elseif sMessage:match(self.L["datachron.locomotion"]) then
+        self.core:AddTimer("Timer_Devourer", self.L["message.devourer"], 68, self.config.timers.devourer)
+        self.core:AddTimer("Timer_Pillar", self.L["message.pillar"], 75, self.config.timers.pillar)
+        self.core:AddTimer("Timer_Laser", self.L["message.laser"], 58, self.config.timers.laser)
+    else
+        local strPlayerFocused = sMessage:match(self.L["datachron.laser"])
+
+        if strPlayerFocused then
+            local tFocusedUnit = GameLib.GetPlayerUnitByName(strPlayerFocused)
+
+            if tFocusedUnit then
+                if tFocusedUnit:IsThePlayer() then
+                    self.core:PlaySound(self.config.sounds.laser)
+                    self.core:ShowAura("Aura_Laser", self.config.auras.laser, 15, self.L["alert.laser_player"])
+                    self.core:ShowAlert("Alert_Laser", self.L["alert.laser_player"], self.config.alerts.laser)
+                else
+                    self.core:DrawIcon("Icon_Laser", tFocusedUnit, self.config.icons.laser, nil, 15)
+                    self.core:ShowAlert("Alert_Laser", self.L["alert.laser"]:format(tFocusedUnit:GetName()), self.config.alerts.laser)
+                end
+            end
+        end
+    end
 end
 
 function Mod:IsRunning()
@@ -57,6 +201,9 @@ end
 
 function Mod:OnEnable()
     self.run = true
+    self.nLastDevourer = 0
+    self.core:AddTimer("Timer_Devourer", self.L["message.devourer"], 10, self.config.timers.devourer)
+    self.core:AddTimer("Timer_Pillar", self.L["message.pillar"], 45, self.config.timers.pillar)
 end
 
 function Mod:OnDisable()
